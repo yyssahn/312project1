27366daaea862d1295b0749d6c341374284e275f
ys <yyssahn@gmail.com>
2014-10-28 17:32:17 -0700

    some more things added

--- files/crusher_r4j8.hs
+++ files/crusher_r4j8.hs
@@ -1,13 +1,45 @@
 
 testBoard = ["WWW-WW-------BB-BBB"]
 testBoardState =["WWW","-WW-", "-----","-BB-","BBB"]
+
 type Pawn = (Char,Int,Int)
-crusher_r4j8 ::[String]->Char->Int->Int->[[String]]
-crusher_r4j8 state team numMove numN = reverse(state_search_r4j8 [state] team numMove numN)
+
+testPawn = ('W',0,0)
+testPawnList = [('W',0,0),('W',0,1),('W',0,2),('W',1,1),('W',1,2),('B',3,1),('B',3,2),('B',4,0),('4',4,1),('B',4,2)]
 
 
-state_search_r4j8 ::[[String]] ->Char->Int->Int->[[String]]
+topleft::Int
+topleft = 0
+topright::Int
+topright = 1 
+left::Int
+left = 2
+right::Int
+right = 3
+bottomleft::Int
+bottomleft = 4
+bottomright::Int
+bottomright = 5
+
+
+
+crusher_r4j8 ::[String]->Char->Int->Int->[String]
+crusher_r4j8 state team numMove numN = reverse(state_search_r4j8 state team numMove numN)
+
+
+state_search_r4j8 ::[String] ->Char->Int->Int->[String]
 state_search_r4j8 path team numMove numN  = path
+
+get_pawn_count::[String]->Char->Int
+get_pawn_count state pawn
+	|	null state = 0
+	|	otherwise = (get_pawn_count_helper (head state) pawn) + get_pawn_count (tail state) pawn
+
+get_pawn_count_helper::[Char]->Char->Int
+get_pawn_count_helper state pawn
+	|	null state = 0
+	|	(head state) ==pawn = 1 + get_pawn_count_helper (tail state) pawn
+	|	otherwise = get_pawn_count_helper (tail state) pawn
 
 make_board :: [Char]->Int->Int->[String]
 make_board input n helper
@@ -26,6 +58,10 @@ make_PawnList state yhelper
 	| null state = []
 	| otherwise = (make_PawnList_helper (head state) 0 yhelper) ++ (make_PawnList (tail state) (yhelper +1))
 
+make_into_single_string :: [String] -> String
+make_into_single_string input
+	| 	null input = []
+	| 	otherwise = (head input) ++ (make_into_single_string (tail input))
 
 make_PawnList_helper :: [Char]->Int->Int->[Pawn]
 make_PawnList_helper str helper y
@@ -54,6 +90,52 @@ mynthtail n list1
 	| otherwise = mynthtail (n-1) (tail list1)
 
 
+empty :: [String]->Int->Int->Bool
+empty state x y
+	|  x<0 || y < 0	= False
+	|	(get_Char state x y) == '-' =True
+	|	otherwise = False
+
+
+possible_move ::[String]->Pawn->Int->Int->Bool
+possible_move state pawn size dir
+	|	get_pawn_y pawn < (size-1) = possible_move_up state pawn dir
+	|	get_pawn_y pawn == (size-1) = possible_move_center state pawn dir
+	|	get_pawn_y pawn >= size = possible_move_down state pawn dir
+
+possible_move_up ::[String]->Pawn->Int->Bool
+possible_move_up state pawn dir
+	|	dir == topleft  = empty state (get_pawn_x pawn - 1) (get_pawn_y pawn - 1)
+	|	dir == topright = empty state (get_pawn_x pawn) (get_pawn_y pawn - 1)
+	|	dir == left = empty state (get_pawn_x pawn - 1) (get_pawn_y pawn)
+	|	dir == right = empty state (get_pawn_x pawn + 1) (get_pawn_y pawn )
+	|	dir == bottomleft = empty state (get_pawn_x pawn) (get_pawn_y pawn + 1)
+	|	dir == bottomright = empty state (get_pawn_x pawn + 1) (get_pawn_y pawn + 1)
+
+
+possible_move_center::[String]->Pawn->Int->Bool
+possible_move_center state pawn dir
+	|	dir == topleft  = empty state (get_pawn_x pawn - 1) (get_pawn_y pawn - 1)
+	|	dir == topright = empty state (get_pawn_x pawn) (get_pawn_y pawn - 1)
+	|	dir == left = empty state (get_pawn_x pawn - 1) (get_pawn_y pawn)
+	|	dir == right = empty state (get_pawn_x pawn + 1) (get_pawn_y pawn )
+	|	dir == bottomleft = empty state (get_pawn_x pawn - 1) (get_pawn_y pawn + 1)
+	|	dir == bottomright = empty state (get_pawn_x pawn) (get_pawn_y pawn + 1)
+
+
+possible_move_down ::[String]->Pawn->Int->Bool
+possible_move_down state pawn dir
+	|	dir == topleft  = empty state (get_pawn_x pawn) (get_pawn_y pawn - 1)
+	|	dir == topright = empty state (get_pawn_x pawn+1) (get_pawn_y pawn - 1)
+	|	dir == left = empty state (get_pawn_x pawn - 1) (get_pawn_y pawn)
+	|	dir == right = empty state (get_pawn_x pawn + 1) (get_pawn_y pawn )
+	|	dir == bottomleft = empty state (get_pawn_x pawn - 1) (get_pawn_y pawn + 1)
+	|	dir == bottomright = empty state (get_pawn_x pawn) (get_pawn_y pawn + 1)
+	
+	
+--
+-- Pawn getters
+--
 get_pawn_char :: Pawn->Char
 get_pawn_char (a,b,c)= a
 get_pawn_x :: Pawn->Int
